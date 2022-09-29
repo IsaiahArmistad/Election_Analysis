27d2766383c764d35aa87857c99ea3937d3afe85
Isaiah Armistad <iarmistad31@gmail.com>
2022-09-29 12:26:09 -0400

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -1,15 +1,58 @@
 # The data we need to retrieve
 import csv
 import os
-
+#Assign variables to load and save a file from a path
 file_to_load = os.path.join("Resources","election_results.csv")
 file_to_save = os.path.join("analysis","election_analysis.txt")
+
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
     headers = next(file_reader)
-    print(headers)
+
+    for row in file_reader:
+        total_votes += 1
+        #print candidate name from each row
+        candidate_name = row[2]
+
+        #if the candidate does not match any existing candidate
+        if candidate_name not in candidate_options:
+            #add the candidate name to the candidate list
+            candidate_options.append(candidate_name)
+            #begin tracking the candidate's vote counts
+            candidate_votes[candidate_name] = 0
+        #add a vote to that candidate's count
+        candidate_votes[candidate_name] += 1
+
+for candidate_name in candidate_votes:
+    votes = candidate_votes[candidate_name]
+    vote_percentage = float(votes)/float(total_votes)*100
+
+    #print out each candidate's name, vote, and percentage of votes
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")    
+
+    #determine wiining vote count and candidate
+    #determine if the votes is greater than the winning count
+    if (votes>winning_count) and (vote_percentage>winning_percentage):
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate_name
+
+winning_candidate_summary = (f"-------------------------\n"
+f"Winner: {winning_candidate}\n"
+f"Winning Vote Count: {winning_count:,}\n"
+f"Winning Percentage: {winning_percentage:.1f}%\n"
+f"-------------------------\n")
+
+print(winning_candidate_summary)
 
 # 2. A complete list of candidates who received votes
 # 3. The percentage of votes each candidate won
